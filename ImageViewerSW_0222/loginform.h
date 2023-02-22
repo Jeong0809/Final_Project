3b2e12c18b666c0dacd3776416f4a8eed1cca355
Jeong0809 <115696581+Jeong0809@users.noreply.github.com>
2023-02-22 20:19:10 +0900

    2023-02-22

---
+++ ImageViewerSW_0222/loginform.h
@@ -0,0 +1,43 @@
+/*프로그램명 : ImageViewerSW
+파일명 : loginform.h
+설명 : 뷰어 SW 프로그램 시작 시 의사ID와 의사명으로 로그인 후 다음창으로 넘어갈 수 있도록 구성된 클래스
+작성자 : 이정연
+최종 수정 날짜 : 2023.02.11*/
+
+#ifndef LOGINFORM_H
+#define LOGINFORM_H
+
+#include <QWidget>
+#include <QMessageBox>
+
+namespace Ui {
+class LoginForm;
+}
+
+class LoginForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit LoginForm(QWidget *parent = nullptr);
+    ~LoginForm();
+
+private slots:
+    void on_LogIn_clicked();
+    void on_quit_clicked();
+    void receiveLogInCheck(QString);
+    void serverConnected(int);
+
+signals:
+    void SendDoctorCheck(QString);
+    void LogInSuccess(QString, QString);
+    void LoginToServer();
+    void reconnect(QString,int);
+
+private:
+    Ui::LoginForm *ui;
+    QString Check;
+    QMessageBox* messagebox;
+};
+
+#endif // LOGINFORM_H
---
+++ ImageViewerSW_0222/mainwindow.h
@@ -0,0 +1,53 @@
+/*프로그램명 : ImageViewerSW
+파일명 : mainwindow.h
+설명 : 각 클래스들 간의 signal-slot을 연결해주는 connect문을 작성한 부분
+stackedWidget을 통해 login창과 viewer창으로 구분되어 있는 클래스
+작성자 : 이정연
+최종 수정 날짜 : 2023.02.11*/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QScrollArea>
+#include <QWheelEvent>
+#include <QGraphicsView>
+#include <QObject>
+#include <QListWidgetItem>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class ImageAlbum;
+class PatientInfo;
+class NetworkManager;
+class LoginForm;
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QImage *img;
+    ImageAlbum* m_imageAlbum;
+    PatientInfo* m_patientInfo;
+    NetworkManager* m_networkManager;
+    LoginForm* m_logIn;
+
+private slots:
+    void on_quit_clicked();
+    void on_Print_clicked();
+    void ViewerOpen(QString, QString);
+
+signals:
+    void sendPrintStart();
+};
+#endif // MAINWINDOW_H
